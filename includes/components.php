f7b6cfa337d2f7094588383cfbe5168e32a4f138
alexangc <alexis.miranda@hotmail.fr>
2020-06-04 08:50:39 +0200

    Re-organized plugin's code.

---
+++ includes/components.php
@@ -0,0 +1,30 @@
+<?php
+defined('ABSPATH') || exit();
+require_once dirname(__FILE__) .
+    '/bp-photo-cat-admin-settings/categories-editor.php';
+
+/**
+ * Central file importing the different components of the plugin.
+ */
+
+// Plugin's global CSS
+if (!function_exists('PHOTOCAT_admin_enqueue_script')) {
+    function PHOTOCAT_admin_enqueue_script()
+    {
+        wp_enqueue_style(
+            'buddyboss-addon-admin-css',
+            plugin_dir_url(__FILE__) . 'style.css'
+        );
+    }
+
+    add_action('admin_enqueue_scripts', 'PHOTOCAT_admin_enqueue_script');
+}
+
+// BuddyBoss's admin panel integration
+function PHOTOCAT_register_integration()
+{
+    require_once dirname(__FILE__) .
+        '/bp-photo-cat-integration/buddyboss-integration.php';
+    buddypress()->integrations['addon'] = new PHOTOCAT_BuddyBoss_Integration();
+}
+add_action('bp_setup_integrations', 'PHOTOCAT_register_integration');
