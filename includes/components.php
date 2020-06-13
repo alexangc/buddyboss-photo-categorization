745c06e41bfb4f425e25f248e204a74dafd43af1
alexangc <alexis.miranda@hotmail.fr>
2020-06-13 18:51:49 +0200

    Added a Wordpress shortcode for the photo gallery widget.

--- includes/components.php
+++ includes/components.php
@@ -6,6 +6,7 @@ require_once dirname(__FILE__) .
     '/bp-photo-cat-upload-categories/upload-categories.php';
 require_once dirname(__FILE__) .
     '/bp-photo-cat-on-photo-upload/upload-actions.php';
+require_once dirname(__FILE__) . '/bp-photo-cat-gallery/photo-gallery.php';
 
 /**
  * Central file importing the different components of the plugin.
@@ -36,3 +37,5 @@ add_action('bp_setup_integrations', 'PHOTOCAT_register_integration');
 add_action('bp_media_add_handler', 'PHOTOCAT_on_photo_upload');
 
 add_filter('bp_get_template_part', 'PHOTOCAT_uploader_categories', 10, 3);
+
+add_shortcode('PHOTOCAT_gallery_shortcode', 'PHOTOCAT_gallery');
