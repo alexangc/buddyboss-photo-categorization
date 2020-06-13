e217fc68e26ce01239244235b620f3b985979a3b
alexangc <alexis.miranda@hotmail.fr>
2020-06-13 17:52:35 +0200

    Formatted file.

--- includes/bp-photo-cat-on-photo-upload/upload-actions.php
+++ includes/bp-photo-cat-on-photo-upload/upload-actions.php
@@ -1,26 +1,31 @@
 <?php
 require_once dirname(__FILE__) . '/../database.php';
 
-function PHOTOCAT_on_photo_upload($params) {
+function PHOTOCAT_on_photo_upload($params)
+{
+    if (count($_POST['medias']) > 0) {
+        $categories = $_POST['medias'][0]['categories'];
+        $tags = [];
 
-  if (count($_POST['medias']) > 0) {
-    $categories = $_POST['medias'][0]['categories'];
-    $tags = array();
+        foreach ($categories as $cat) {
+            $tags[] = PHOTOCAT_tagify(sanitize_text_field($cat['value']));
+        }
 
-    foreach ($categories as $cat) {
-      $tags[] = PHOTOCAT_tagify(sanitize_text_field($cat['value']));
+        foreach ($params as $media_id) {
+            PHOTOCAT_insert_photo_categories($media_id, $tags);
+        }
     }
 
-    foreach ($params as $media_id) {
-      PHOTOCAT_insert_photo_categories($media_id, $tags);
-    }
-  }
-
-  return $params;
+    return $params;
 }
 
-function PHOTOCAT_tagify($string) {
-  $string = strtr(utf8_decode($string), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
-  return strtolower(preg_replace('/[^a-zA-Z0-9]+/','-', $string));
+function PHOTOCAT_tagify($string)
+{
+    $string = strtr(
+        utf8_decode($string),
+        utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'),
+        'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY'
+    );
+    return strtolower(preg_replace('/[^a-zA-Z0-9]+/', '-', $string));
 }
 ?>
